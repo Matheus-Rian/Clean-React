b40827938e856f1b902ba33250fa27737c806294
Matheus Rian <souzamatheusrian@gmail.com>
2021-09-29 15:04:52 -0300

    feat: add authentication usecase

---
+++ src/domain/usecases/authenticator.ts
@@ -0,0 +1,11 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
+import { AccountModel } from '../models/account-model'
+
+type AuthenticationParams = {
+  email: string
+  password: string
+}
+
+export interface Authentication {
+  auth (params: AuthenticationParams): Promise<AccountModel>
+}
