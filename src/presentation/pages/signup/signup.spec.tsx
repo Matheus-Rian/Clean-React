b6cdb78eefbeab6ae9a9c6bb6b5c85674dbcc83a
Matheus Rian <souzamatheusrian@gmail.com>
2022-10-22 09:36:34 -0300

    test: ensure SignUp change password status to success if Validation succeeds

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -82,4 +82,10 @@ describe('SignUp Component', () => {
     Helper.populateField(sut, 'email')
     Helper.testStatusForField(sut, 'email')
   })
+
+  test('Should show valid password state if Validation succeeds', () => {
+    const { sut } = makeSut()
+    Helper.populateField(sut, 'password')
+    Helper.testStatusForField(sut, 'password')
+  })
 })
