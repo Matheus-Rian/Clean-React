e2454aed60a5c8d66ff36a0c286268ab1074402f
Matheus Rian <souzamatheusrian@gmail.com>
2022-10-23 14:21:54 -0300

    test: ensure SignUp calls AddAccount with correct values

--- src/presentation/pages/signup/signup.spec.tsx
+++ src/presentation/pages/signup/signup.spec.tsx
@@ -1,12 +1,13 @@
 
 import React from 'react'
 import faker from 'faker'
-import { Helper, ValidationStub } from '@/presentation/test'
+import { AddAccountSpy, Helper, ValidationStub } from '@/presentation/test'
 import { cleanup, fireEvent, render, RenderResult, waitFor } from '@testing-library/react'
 import SignUp from './signup'
 
 type SutTypes = {
   sut: RenderResult
+  addAccountSpy: AddAccountSpy
 }
 
 type SutParams = {
@@ -17,14 +18,17 @@ const makeSut = (params?: SutParams): SutTypes => {
   const validationStub = new ValidationStub()
   validationStub.errorMessage = params?.validationError
 
+  const addAccountSpy = new AddAccountSpy()
   const sut = render(
     <SignUp
       validation={validationStub}
+      addAccount={addAccountSpy}
     />
   )
 
   return {
-    sut
+    sut,
+    addAccountSpy
   }
 }
 
@@ -123,5 +127,19 @@ describe('SignUp Component', () => {
     const { sut } = makeSut()
     await simulateValidSubmit(sut)
     Helper.testElementExists(sut, 'spinner')
+  })
+
+  test('Should call AddAccount with correct values', async () => {
+    const { sut, addAccountSpy } = makeSut()
+    const name = faker.name.findName()
+    const email = faker.internet.email()
+    const password = faker.internet.password()
+    await simulateValidSubmit(sut, name, email, password)
+    expect(addAccountSpy.params).toEqual({
+      name,
+      email,
+      password,
+      passwordConfirmation: password
+    })
   })
 })
---
+++ src/presentation/test/mock-add-account.ts
@@ -0,0 +1,14 @@
+import { AccountModel } from '@/domain/models'
+import { mockAccountModel } from '@/domain/test'
+import { AddAccount, AddAccountParams, AuthenticationParams } from '@/domain/usecases'
+
+export class AddAccountSpy implements AddAccount {
+  account = mockAccountModel()
+  params: AuthenticationParams
+  callsCount = 0
+
+  async add (params: AddAccountParams): Promise<AccountModel> {
+    this.params = params
+    return this.account
+  }
+}
