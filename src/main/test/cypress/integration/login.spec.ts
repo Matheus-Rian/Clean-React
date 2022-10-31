2115cc4070579ab30d040d4f29431683a53245c1
Matheus Rian <souzamatheusrian@gmail.com>
2022-10-31 19:54:40 -0300

    test: ensure Login begin with correct initial state

--- src/main/test/cypress/integration/login.spec.ts
+++ src/main/test/cypress/integration/login.spec.ts
@@ -1,5 +1,16 @@
 describe('Login', () => {
-  it('Should load with correct initial state', () => {
+  beforeEach(() => {
     cy.visit('login')
+  })
+
+  it('Should load with correct initial state', () => {
+    cy.getByTestId('email-status')
+      .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('password-status')
+      .should('have.attr', 'title', 'Campo Obrigatório')
+      .should('contain.text', '🔴')
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
   })
 })
