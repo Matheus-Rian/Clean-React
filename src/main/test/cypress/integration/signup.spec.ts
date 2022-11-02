e64ebb54476d7b2ceedb071d0de150b2fea1ef47
Matheus Rian <souzamatheusrian@gmail.com>
2022-11-02 15:45:40 -0300

    test: ensure SignUp present error state if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -1,3 +1,4 @@
+import faker from 'faker'
 import * as FormHelper from '../support/form-helper'
 
 describe('SignUp', () => {
@@ -17,6 +18,23 @@ describe('SignUp', () => {
 
     cy.getByTestId('passwordConfirmation').should('have.attr', 'readOnly')
     FormHelper.testInputStatus('passwordConfirmation', 'Campo Obrigatório')
+
+    cy.getByTestId('submit').should('have.attr', 'disabled')
+    cy.getByTestId('error-wrap').should('not.have.descendants')
+  })
+
+  it('Should present error if form is invalid', () => {
+    cy.getByTestId('name').focus().type(faker.random.alphaNumeric(3))
+    FormHelper.testInputStatus('name', 'Valor inválido')
+
+    cy.getByTestId('email').focus().type(faker.random.word())
+    FormHelper.testInputStatus('email', 'Valor inválido')
+
+    cy.getByTestId('password').focus().type(faker.random.alphaNumeric(4))
+    FormHelper.testInputStatus('password', 'Valor inválido')
+
+    cy.getByTestId('passwordConfirmation').focus().type(faker.random.alphaNumeric(3))
+    FormHelper.testInputStatus('password', 'Valor inválido')
 
     cy.getByTestId('submit').should('have.attr', 'disabled')
     cy.getByTestId('error-wrap').should('not.have.descendants')
