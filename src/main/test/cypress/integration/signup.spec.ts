f8f5bbe25f8514a4c72fac8c4e1cceeefd9419fd
Matheus Rian <souzamatheusrian@gmail.com>
2022-11-02 17:11:27 -0300

    test: ensure SignUp prevent submit if form is invalid

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -121,4 +121,11 @@ describe('SignUp', () => {
     cy.getByTestId('submit').dblclick()
     FormHelper.testHttpCallsCount(1)
   })
+
+  it('Should not call submit if form is invalid', () => {
+    Http.mockOk()
+
+    cy.getByTestId('email').focus().type(faker.internet.email()).type('{enter}')
+    FormHelper.testHttpCallsCount(0)
+  })
 })
