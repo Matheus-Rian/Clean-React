487794797b206ff23e5c15f04797e2b4b37bc25d
Matheus Rian <souzamatheusrian@gmail.com>
2022-11-02 17:06:55 -0300

    test: ensure SignUp prevent multiple submits

--- src/main/test/cypress/integration/signup.spec.ts
+++ src/main/test/cypress/integration/signup.spec.ts
@@ -2,13 +2,17 @@ import faker from 'faker'
 import * as FormHelper from '../support/form-helper'
 import * as Http from '../support/signup-mocks'
 
-const simulateValidSubmit = (): void => {
+const populateFields = (): void => {
   const password = faker.random.alphaNumeric(6)
 
   cy.getByTestId('name').focus().type(faker.name.findName())
   cy.getByTestId('email').focus().type(faker.internet.email())
   cy.getByTestId('password').focus().type(password)
   cy.getByTestId('passwordConfirmation').focus().type(password)
+}
+
+const simulateValidSubmit = (): void => {
+  populateFields()
   cy.getByTestId('submit').click()
 }
 
@@ -109,5 +113,12 @@ describe('SignUp', () => {
 
     FormHelper.testUrl('/')
     FormHelper.testLocalStorage('accessToken')
+  })
+
+  it('Should prevent multiple submits', () => {
+    Http.mockOk()
+    populateFields()
+    cy.getByTestId('submit').dblclick()
+    FormHelper.testHttpCallsCount(1)
   })
 })
--- src/main/test/cypress/support/signup-mocks.ts
+++ src/main/test/cypress/support/signup-mocks.ts
@@ -4,4 +4,4 @@ import * as Helper from './http-mocks'
 export const mockEmailInUseError = (): void => Helper.mockEmailInUseError(/signup/)
 export const mockUnexpectedError = (): void => Helper.mockUnexpectedError(/signup/, 'POST')
 export const mockInvalidData = (): void => Helper.mockOk(/signup/, 'POST', { invalidProperty: faker.datatype.uuid() })
-export const mockOk = (): void => Helper.mockOk(/login/, 'POST', { accessToken: faker.datatype.uuid() })
+export const mockOk = (): void => Helper.mockOk(/signup/, 'POST', { accessToken: faker.datatype.uuid() })
